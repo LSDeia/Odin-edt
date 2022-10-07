814f8e8f3f906a361c4893476c1a00fbda01eeae
RemRem <remrem@github.com>
2022-10-07 13:15:02 +0200

    make sed works

--- pretty.sh
+++ pretty.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-FILE='test.txt' #have to be replace by scheduled.html
+FILE='schedule.html' #have to be replace by scheduled.html
 FORMATED='pretty-scheduled.txt'
-
-#file=$(cat $FILE | sed 's/<[^>]*>//g;s/&nbsp//g;s/;//g;s/salle: //g;s/enseignant://g;s/enseignants://g;s/  //g;/^$/d')
-#Mets tout le texte sur une ligne quand récupéré dans une variable
+CLEANFILE='clean-scheduled.txt'
+#Delete all 
+cat $FILE | sed 's/<[^>]*>//g;s/&nbsp//g;s/;//g;s/salle: //g;s/enseignant://g;s/enseignants://g;s/  //g;/^$/d' > $CLEANFILE
 
 while IFS= read -r line; do #pas trop compris (stackexchange)
     if [[ $line =~ ^[A-Z][a-z]{4,7}[0-9]{2} ]]; then #if line is a date
@@ -14,4 +14,4 @@ while IFS= read -r line; do #pas trop compris (stackexchange)
     else
         echo -n " $line " >> $FORMATED
     fi
-done < "$FILE"
+done < "$CLEANFILE"
